3f819cad6ea943d209fd23397fb14ed4ce874762
Jose Antonio Reyes Lecuona <joseantonioreyeslecuona@gmail.com>
2026-07-04 21:06:50 +0200

    Hand script
    Hand script + card idle animation

--- bombs/Assets/Scripts/CardDisplay.cs
+++ bombs/Assets/Scripts/CardDisplay.cs
@@ -4,24 +4,92 @@ using TMPro;
 
 public class CardDisplay : MonoBehaviour
 {
+
+    [Header("Card Info - Scriptable object")]
+    [Tooltip("Scriptable object instance of the card")]
     public Card cardData;
 
+    [Header("Prefab elements - DO NOT MODIFIED")]
     public TMP_Text title;
     public TMP_Text description;
-   
-
     public Image cardImage;
+
+    [Header("Idle Animation")]
+    public bool hasIdleAnimation = false;
+    [Range(0f, 1f)]
+    public float scaleModifyIntensity = 0.05f;
+    [Range(0f, 20f)]
+    public float rotationIntensity = 10f;
+    public float scaleCicleTime = 1f;
+    public float rotationCicleTime = 1f;
+
+
+    // Values for base
+    private Quaternion baseRotation;
+    private Vector3 baseScale;
 
     void Start()
     {
+        
+        baseRotation = transform.localRotation;
+        baseScale = transform.localScale;
+
         UpdateCardDisplay();
+        
+    }
+    private void Update()
+    {
+        if (hasIdleAnimation) CodeAnimation();
     }
 
-    
+
     public void UpdateCardDisplay()
     {
         title.text = cardData.cardName;
         description.text = cardData.cardDescription;
         cardImage.sprite = cardData.cardImage;
+    }
+    public void ChangeBaseRotation(Quaternion newRotation)
+    {
+        baseRotation = newRotation;
+     
+    }
+
+    public void CodeAnimation()
+    {
+        //calculate angle from 2 pi
+        float waveScaleAmplitud = (Time.time / scaleCicleTime) * Mathf.PI * 2f;
+
+        transform.localScale = new Vector3(
+          baseScale.x,
+          baseScale.y + baseScale.y * scaleModifyIntensity * Mathf.Sin(waveScaleAmplitud),
+          baseScale.z
+        );
+
+        float waveRotationAmplitud = (Time.time / rotationCicleTime) * Mathf.PI * 2f;
+
+        transform.localRotation = baseRotation * Quaternion.Euler(
+            0f,
+            0f,
+            rotationIntensity * Mathf.Sin(waveRotationAmplitud)
+        );
+
+
+
+
+        /*
+        // Normalize sin from -1 - 1 to 0-1
+        float normalized = (Mathf.Sin(waveAmplitud) + 1f )/2f;
+
+        // Apply the Intensity
+        float scaleFactor = Mathf.Lerp(1f - scaleModifyIntensity, 1f, normalized);
+
+        //Aply Scale transform
+        transform.localScale = new Vector3(
+            baseScale.x,
+            baseScale.y * scaleFactor,
+            baseScale.z
+        );
+        */
     }
 }
---
+++ bombs/Assets/Scripts/HandManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+public class HandManager : MonoBehaviour
+{
+    public GameObject cardPrefab;
+
+    public Transform handTransform; //Root
+
+    public float fanSpreadAngle = 5f;
+    public int fanSpreadXOffset = 50;
+    public int fanSpreadYOffset = 50;
+
+    public List<GameObject> cardInHand = new List<GameObject>();
+    void Start()
+    {
+        AddCardsToHand();
+    }
+
+    
+    public void AddCardsToHand()
+    {
+        GameObject newCard = Instantiate(cardPrefab, handTransform.position, Quaternion.identity, handTransform);
+        cardInHand.Add(newCard);
+        UpdateHandVisuals();
+    }
+    public void UpdateHandVisuals()
+    {
+        int cardCount = cardInHand.Count;
+        
+
+        for (int i = 0; i < cardCount; i++)
+        {
+            float rotationAngle = (fanSpreadAngle * ((i + 0.5f) - (cardCount /2f)));
+            cardInHand[i].transform.localRotation = Quaternion.Euler(0f, 0f, -rotationAngle);
+            cardInHand[i].GetComponent<CardDisplay>().ChangeBaseRotation(Quaternion.Euler(0f, 0f, -rotationAngle));
+            cardInHand[i].transform.localPosition = new Vector3 (((i + 0.5f) - (cardCount / 2f))* fanSpreadXOffset, -Mathf.Abs((i+0.5f) - (cardCount / 2f)) * fanSpreadYOffset, 0);
+        }
+
+    }
+  
+    public void Update() {
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            AddCardsToHand();
+        } 
+    }
+
+}
+       
